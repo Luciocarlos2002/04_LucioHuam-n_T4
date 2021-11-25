95ce8e4259c7b25abadc5eee1cc2c1be52c45f09
LucioHuaman <lucio.huaman@vallegrande.edu.pe>
2021-11-25 00:24:20 -0500

    Criptografia v4

--- src/main/java/CifradoCesar.java
+++ src/main/java/CifradoCesar.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 
+    /**
+     * Cifrado Sustitución
+     * Creamos una clase CifradoCesar para la encriptación y descencriptación.
+     */
 public class CifradoCesar {
 
     public static void main(String[] args) {
-
+        /**
+         * Creamos una clase Scanner para leer la entrada del teclado.
+         */
         Scanner sn = new Scanner(System.in);
         sn.useDelimiter("\n");
-
+        /**
+         * Insertamos las cadenas y lo guardamos en un String.
+         */
         String letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-
+        /**
+         * Insertamos la frase o valor de entrada.
+         */
         System.out.println("Dame una frase");
         String frase = sn.next();
 
@@ -19,18 +29,27 @@ public class CifradoCesar {
         System.out.println("Texto descodificado de : " + texto + "  es  " + texto1);
 
     }
-
+        /**
+         * Creamos el método codificar.
+         */
     public static String codificar(String letras, String texto){
         String textoCodificado = "";
-
+        /**
+         * El texto codificado se imprime en mayuscula.
+         */
         texto = texto.toUpperCase();
 
+        /**
+         * Recorremos la cadena con un bucle.
+         */
         char caracter;
         for (int i = 0; i < texto.length(); i++) {
             caracter = texto.charAt(i);
 
             int pos = letras.indexOf(caracter);
-
+            /**
+             * El texto que vamos a decodificar se encuentre en nuestra cadena.
+             */
             if(pos == -1){
                 textoCodificado += caracter;
             }else{
@@ -41,18 +60,26 @@ public class CifradoCesar {
 
         return textoCodificado;
     }
-
+        /**
+         * Creamos el método descodificar.
+         */
     public static String descodificar(String letras, String texto1){
         String textoDescodificado = "";
-
+        /**
+         * El texto descodificado se imprime en mayuscula.
+         */
         texto1 = texto1.toUpperCase();
-
+        /**
+         * Recorremos la cadena descodificada con un bucle.
+         */
         char caracter;
         for (int i = 0; i < texto1.length(); i++) {
             caracter = texto1.charAt(i);
 
             int pos = letras.indexOf(caracter);
-
+            /**
+             * El texto decodificado se encuentre en el texto que ingresamos al inicio.
+             */
             if(pos == -1){
                 textoDescodificado += caracter;
             }else{
--- src/main/java/DES.java
+++ src/main/java/DES.java
@@ -9,20 +9,35 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 
+    /**
+     * Creamos una clase DES para la encriptación y descencriptación.
+     */
 public class DES {
+    /**
+     * Creamos el método encryption para nuestro archivo.
+     */
     public static void encryption()
             throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException
     {
         try
         {
+            /**
+             * Creamos nuestro key y especificamos el tipo de algoritmo DES.
+             */
             Cipher cipher=Cipher.getInstance("DES");
             KeyGenerator keyg=KeyGenerator.getInstance("DES");
             Key key=keyg.generateKey();
-
+            /**
+             * Creamos el ENCRYPT_MODE.
+             */
             cipher.init(Cipher.ENCRYPT_MODE, key);
-
+            /**
+             * Insertamos la ubicación de nuestro archivo.
+             */
             CipherInputStream cipt=new CipherInputStream(new FileInputStream(new File("C:\\Users\\Carlos\\Desktop\\04_LucioHuaman_T4\\LucioHuaman.jpg")), cipher);
-
+            /**
+             * Guardamos el archivo encriptado en la misma ubicación con tipo de archivo .txt
+             */
             FileOutputStream fileip=new FileOutputStream(new File("C:\\Users\\Carlos\\Desktop\\04_LucioHuaman_T4\\encrypt.txt"));
             System.err.println("Imagen Ecriptada dentro de la carpeta - 04_LucioHuamán_T4");
 
@@ -32,11 +47,17 @@ public class DES {
                 fileip.write(i);
 
             }
-
+            /**
+             * Creamos el DECRYPT_MODE.
+             */
             cipher.init(Cipher.DECRYPT_MODE, key);
-
+            /**
+             * Insertamos la ubicación de nuestro archivo encriptado.
+             */
             CipherInputStream ciptt=new CipherInputStream(new FileInputStream(new File("C:\\Users\\Carlos\\Desktop\\04_LucioHuaman_T4\\encrypt.txt")), cipher);
-
+            /**
+             * Guardamos el archivo descencriptado en la misma ubicación con tipo de archivo .jpg
+             */
             FileOutputStream fileop=new FileOutputStream(new File("C:\\Users\\Carlos\\Desktop\\04_LucioHuaman_T4\\decrypt.jpg"));
             System.err.println("Descencriptando la imagen Encriptada en la carpeta - 04_LucioHuamán_T4");
 
@@ -53,7 +74,9 @@ public class DES {
         }
 
     }
-
+        /**
+         * Creamos una clase principal para imprimir nuestros métodos
+         */
     public static void main(String[] args)
     {
         try
